5c345f3906b9b7fa2b6e1d53c9a61868a58330fc
AC <ac@marketmining.com.br>
2023-04-20 18:29:25 -0300

    setup db

--- src/main/resources/data-dev.sql
+++ src/main/resources/data-dev.sql
@@ -1,2 +1,3 @@
 insert into pessoas(id, nome_completo, idade) values (nextval('hibernate_sequence'), 'AC', 30);
-insert into pessoas(id, nome_completo, idade) values (nextval('hibernate_sequence'), 'Pelé', 15);
+insert into pessoas(id, nome_completo, idade) values (nextval('hibernate_sequence'), 'Pelé', 50);
+insert into pessoas(id, nome_completo, idade) values (nextval('hibernate_sequence'), 'Messi', 28);
